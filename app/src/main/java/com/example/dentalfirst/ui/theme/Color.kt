61a69661c7d82d547f873e72aeeba134a2810e18
Ilya Shevtsov <shevtsovis@novotelecom.ru>
2026-02-12 13:25:54 +0800

    initial commit

---
+++ app/src/main/java/com/example/dentalfirst/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.dentalfirst.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val DarkGrey = Color(0xFF1A1A1A)
+val MiddleGrey = Color(0xFF4D4D4D)
+val LightGrey = Color(0xFF999999)
+val SuperLightGrey = Color(0xFFFAFAFA)
+
